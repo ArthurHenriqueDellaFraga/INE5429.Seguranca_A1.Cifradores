d180975b4d2bd34bdd527175ac016d47280016c2
Felipe Calistro <felipechaiben@gmail.com>
2017-03-21 18:07:00 -0300

    Veginere

--- src/cifras/Veginere.java
+++ src/cifras/Veginere.java
@@ -1,6 +1,7 @@
 package cifras;
 
 import modelo.Cifra;
+import utils.AlgoritmoDeCesar;
 
 public class Veginere implements Cifra {
 
@@ -12,12 +13,45 @@ public class Veginere implements Cifra {
 	
 	@Override
 	public String cifrar(String texto) {
-		return null;
+		int tamanhoTexto = texto.length();
+		String chaveExpandida = this.expandirChave(texto.length());
+		String textoCifrado = "";
+		
+		for(int i = 0; i < tamanhoTexto; i++){
+			int offsetChave = this.offsetChave(chaveExpandida.charAt(i));
+			textoCifrado += AlgoritmoDeCesar.cifrar(texto.substring(i, i), offsetChave);
+		}
+		
+		return textoCifrado;
 	}
 
 	@Override
 	public String decifrar(String texto) {
-		return null;
+		int tamanhoTexto = texto.length();
+		String chaveExpandida = this.expandirChave(texto.length());
+		String textoDecifrado = "";
+		
+		for(int i = 0; i < tamanhoTexto; i++){
+			int offsetChave = this.offsetChave(chaveExpandida.charAt(i));
+			textoDecifrado += AlgoritmoDeCesar.decifrar(texto.substring(i, i), offsetChave);
+		}
+		
+		return textoDecifrado;
+	}
+	
+	private String expandirChave(int tamanho){
+		String novaChave = this.chave;
+		int tamanhoChave = this.chave.length();
+		if(this.chave.length() < tamanho ){
+			for(int i = 0; i < tamanho; i++){
+				novaChave += this.chave.charAt(i % tamanhoChave);
+			}
+		}
+		return novaChave;
+	}
+	
+	private int offsetChave(char charAt) {
+		return 0;
 	}
 
 }
